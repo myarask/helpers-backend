f490c422d42d0e44f207e579d10f446c23a7c7db
myarask <myaraskavitch@gobonfire.com>
2021-01-24 11:36:05 -0500

    fdsf

--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -2,14 +2,20 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
+type Root = any;
+
+type Args = {
+  id: number;
+};
+
 const resolvers = {
   Query: {
     services: () => {
       return prisma.services.findMany();
     },
-    visit: (_: any, { id }: { id: number }) => {
+    visit: (_: Root, args: Args) => {
       // SECURITY: Visits should only be readable by participants
-      return prisma.visits.findFirst({ where: { id } });
+      return prisma.visits.findFirst({ where: { id: args.id } });
     },
   },
 };
