a0793ed764fb16da4b1eb6acef5d7fc1e00abd64
myarask <myaraskavitch@gobonfire.com>
2021-01-24 12:46:48 -0500

    integrate auth0

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import * as dotenv from "dotenv";
 import path from "path";
 dotenv.config({ path: path.resolve(__dirname, "../.env") });
 
+import jwt from "express-jwt";
+import jwksRsa from "jwks-rsa";
 import express from "express";
 import { ApolloServer } from "apollo-server-express";
 import typeDefs from "./typeDefs";
@@ -10,9 +12,28 @@ import resolvers from "./resolvers/index";
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  context: ({ req }: any) => {
+    return req; // Includes user identification from auth0
+  },
 });
 
 const app = express();
+
+// Forces calls to have a valid token in the Authorization header
+app.use(
+  jwt({
+    secret: jwksRsa.expressJwtSecret({
+      cache: true,
+      rateLimit: true,
+      jwksRequestsPerMinute: 5,
+      jwksUri: `https://${process.env.TENANT}.auth0.com/.well-known/jwks.json`,
+    }),
+
+    audience: process.env.AUDIENCE,
+    issuer: `https://${process.env.TENANT}.auth0.com/`,
+    algorithms: ["RS256"],
+  })
+);
 server.applyMiddleware({ app, path: "/api/graphql" });
 
 app.listen({ port: 4000 }, () =>
--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -1,14 +1,31 @@
 import { PrismaClient } from "@prisma/client";
 
+type Context = {
+  user: {
+    iss: string;
+    sub: string;
+    aud: Array<string>;
+    iat: number;
+    exp: number;
+    azp: string;
+    scope: string;
+  };
+};
+
 const prisma = new PrismaClient();
 
 const resolvers = {
   Query: {
     services: () => prisma.services.findMany(),
-    visit: (_, args: { id: number }) => {
-      // SECURITY: Visits should only be readable by participants
-      return prisma.visits.findFirst({ where: { id: args.id } });
-    },
+    visit: (_, args: { id: number }, context: Context) =>
+      prisma.visits.findFirst({
+        where: {
+          id: args.id,
+          Users: {
+            auth0Id: context.user.sub, // Only readable by visit creator
+          },
+        },
+      }),
   },
 };
 
