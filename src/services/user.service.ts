24c08e52b18829e1153e0ffe92469d1ac5015d70
myarask <mark@yaraskavitch.com>
2021-03-07 10:04:45 -0500

    touch up email template

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,7 +1,7 @@
 import httpStatus from "http-status";
 import { formatISO } from "date-fns";
 import { hashPassword } from "../utils/userPassword";
-import prismaClient from '../prismaClient'
+import prismaClient from "../prismaClient";
 import { ApiError } from "../utils/catchAsync";
 import emailService from "./email.service";
 
@@ -11,29 +11,37 @@ import emailService from "./email.service";
  * @returns {Promise<User>}
  */
 const createUser = async (userBody) => {
-  const ifExist = await prismaClient.users.findUnique({ where: { email: userBody.email } });
+  const ifExist = await prismaClient.users.findUnique({
+    where: { email: userBody.email },
+  });
   if (ifExist) {
-    throw new ApiError(httpStatus.BAD_REQUEST, 'This email is already in use. Please login');
+    throw new ApiError(
+      httpStatus.BAD_REQUEST,
+      "This email is already in use. Please login"
+    );
   }
 
   if (!userBody.password) {
-    throw new ApiError(httpStatus.BAD_REQUEST, 'Please provide proper password');
+    throw new ApiError(
+      httpStatus.BAD_REQUEST,
+      "Please provide proper password"
+    );
   }
 
-  const slatedPassword = await hashPassword(userBody.password)
+  const slatedPassword = await hashPassword(userBody.password);
   const user = await prismaClient.users.create({
     data: {
       email: userBody.email,
       createdAt: formatISO(new Date()),
       updatedAt: formatISO(new Date()),
-      password: slatedPassword
-    }
-  })
+      password: slatedPassword,
+    },
+  });
 
-  const subject = 'Welcome to getHelpers';
-  const text = 'Welcome email text'
-  // TODO: Update text
-  await emailService.sendEmail(userBody.email, subject, text)
+  const subject = "Welcome to Helpers";
+  const text = "Your account has been created successfully";
+
+  await emailService.sendEmail(userBody.email, subject, text);
 
   return user;
 };
@@ -46,7 +54,7 @@ const createUser = async (userBody) => {
 const getUserByEmail = async (email: string) => {
   const user = await prismaClient.users.findUnique({ where: { email } });
   if (!user) {
-    throw new ApiError(httpStatus.NOT_FOUND, 'User not found');
+    throw new ApiError(httpStatus.NOT_FOUND, "User not found");
   }
   return user;
 };
@@ -56,10 +64,10 @@ const getUserByEmail = async (email: string) => {
  * @param {number} email
  * @returns {Promise<User>}
  */
-const getUserById = async (id : number) => {
+const getUserById = async (id: number) => {
   const user = await prismaClient.users.findUnique({ where: { id } });
   if (!user) {
-    throw new ApiError(httpStatus.NOT_FOUND, 'User not found');
+    throw new ApiError(httpStatus.NOT_FOUND, "User not found");
   }
   return user;
 };
@@ -70,15 +78,13 @@ const getUserById = async (id : number) => {
  * @param {number} userBody
  * @returns {Promise<User>}
  */
-const updateUserById = async (id : number, userBody: any) => {
+const updateUserById = async (id: number, userBody: any) => {
   const user = await prismaClient.users.findUnique({ where: { id } });
   if (!user) {
-    throw new ApiError(httpStatus.NOT_FOUND, 'User not found');
+    throw new ApiError(httpStatus.NOT_FOUND, "User not found");
   }
   return prismaClient.users.update({ where: { id }, data: userBody });
 };
-
-
 
 const userService = {
   createUser,
